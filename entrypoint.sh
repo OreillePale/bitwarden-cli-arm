fdeb0dd50d07b2d6d5f3d1f879a026946e75eb88
Etienne Batori <43140184+OreillePale@users.noreply.github.com>
2025-09-30 09:03:06 +0200

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+bw config server ${BW_HOST}
+
+if [ -n "$BW_CLIENTID" ] && [ -n "$BW_CLIENTSECRET" ]; then
+    echo "Using apikey to log in"
+    bw login --apikey --raw
+    export BW_SESSION=$(bw unlock --passwordenv BW_PASSWORD --raw)
+else
+    echo "Using username and password to log in"
+    export BW_SESSION=$(bw login ${BW_USER} --passwordenv BW_PASSWORD --raw)
+fi
+
+bw unlock --check
+
+echo 'Running `bw server` on port 8087'
+bw serve --hostname 0.0.0.0 #--disable-origin-protection
